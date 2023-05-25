3ab94f4accf66933de74f76a36ce2b173b330629
LiXizhi <lixizhi@yeah.net>
2023-05-25 10:53:23 +0800

    minor fix

--- Mod/NPLLauncher/main_loop.lua
+++ Mod/NPLLauncher/main_loop.lua
@@ -12,14 +12,26 @@ local main_state = nil;
 NPL.load("./gui_helper.lua")
 
 local function InitApp()
-	local width, height = 500, 400
-	local _this = ParaUI.CreateUIObject("button", "my_button", "_ct", -width/2, -height/2, width, height);
-	_this.text = "HelloWorld from NPL Launcher";
+	local _this = ParaUI.CreateUIObject("container", "bg", "_fi", 0,0,0,0);
 	_this.background = "Texture/whitedot.png";
-	_guihelper.SetUIColor(_this, "255 0 0")
-	_guihelper.SetButtonTextColor(_this, "255 255 255")
-
+	_guihelper.SetUIColor(_this, "0 0 0")
 	_this:AttachToRoot();
+	local _parent = _this;
+
+	local width, height = 500, 64
+	local _this = ParaUI.CreateUIObject("button", "my_button", "_ct", -width/2, -height/2 - 80, width, height);
+	_this.font = "System;18;bold";
+	_this.text = "Made with Paracraft";
+	_this.background = "Texture/whitedot.png";
+	_guihelper.SetUIColor(_this, "128 128 128")
+	_guihelper.SetButtonTextColor(_this, "255 255 255")
+	_parent:AddChild(_this);
+
+	local _this = ParaUI.CreateUIObject("button", "progressText", "_ct", -width/2, -height/2, width, height);
+	_this.font = "System;18;bold";
+	_this.text = "正在加载：0%";
+	_guihelper.SetButtonTextColor(_this, "255 255 255")
+	_parent:AddChild(_this);
 end
 
 local function activate()
